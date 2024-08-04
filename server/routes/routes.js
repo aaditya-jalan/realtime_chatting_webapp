13fdaf1fb6093aed85234a2f062eeabb518b026a
aaditya-jalan <134124151+aaditya-jalan@users.noreply.github.com>
2024-08-04 11:20:07 +0530

    changed files

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,7 +4,7 @@ import { newConversation } from '../controller/conversation-controller.js';
 import { getConversation } from '../controller/conversation-controller.js';
 import { newMessage } from '../controller/message-controller.js';
 import { getMessages } from '../controller/message-controller.js';
-import { uploadImage } from '../controller/image-controller.js';
+//import { uploadImage } from '../controller/image-controller.js';
 //must write .js above
 const route=express.Router();
 //we are creating a post route with endpoint /add on our express server  and the callback function is what we do when this route is hit
@@ -15,7 +15,7 @@ route.post('/conversation/add',newConversation);
 route.post('/conversation/get',getConversation);
 route.post('/message/add',newMessage);
 route.get('/message/get/:id',getMessages);
-route.post('/file/upload',upload.single("file"),uploadFile);//passing a middleware into as second argument
-route.get('/file/:filename', getImage);
+//route.post('/file/upload',upload.single("file"),uploadFile);//passing a middleware into as second argument
+//route.get('/file/:filename', getImage);
 
 export default route;
--- socket/index.js
+++ socket/index.js
@@ -1,45 +1,47 @@
-import { Server } from 'socket.io';
+import { Server } from "socket.io";
 
-const io = new Server(9000, {
+const io = new Server(9000, { // first is port, second is an object
     cors: {
-        origin: 'http://localhost:3000',
-    }, 
-})
-
+        origin: 'http://localhost:3000'
+    }
+});
 
 let users = [];
 
 const addUser = (userData, socketId) => {
-    !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
-}
-
-const removeUser = (socketId) => {
-    users = users.filter(user => user.socketId !== socketId);
-}
+    if (!users.some(user => user.sub === userData.sub)) {
+        users.push({ ...userData, socketId });
+    }
+};
 
 const getUser = (userId) => {
     return users.find(user => user.sub === userId);
-}
+};
 
-io.on('connection',  (socket) => {
-    console.log('user connected')
+io.on('connection', (socket) => {
+    console.log('socket user connected');
 
-    //connect
-    socket.on("addUser", userData => {
+    socket.on("addUsers", userData => {
         addUser(userData, socket.id);
         io.emit("getUsers", users);
-    })
+    });
 
-    //send message
-    socket.on('sendMessage', (data) => {
-        const user = getUser(data.receiverId);
-        io.to(user.socketId).emit('getMessage', data)
-    })
+    socket.on("sendMessage", (data) => {
+        const user = getUser(data.receieverId);
+        if (user) {
+            io.to(user.socketId).emit('getMessage', data);
+        } else {
+            console.log(`User not found for receiverId: ${data.receieverId}`);
+        }
+    });
 
-    //disconnect
+    
+
     socket.on('disconnect', () => {
-        console.log('user disconnected');
-        removeUser(socket.id);
-        io.emit('getUsers', users);
-    })
-})
+        users = users.filter(user => user.socketId !== socket.id);
+        io.emit("getUsers", users);
+        console.log(`User disconnected: ${socket.id}`);
+    });
+});
+
+console.log("Server is running on port 9000");
